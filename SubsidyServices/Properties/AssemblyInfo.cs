56ccc7defe42a22ed64df010c8e0059e7e306804
Mohm4Z <mohm4z@live.com>
2018-08-13 00:28:26 +0300

    Log4net

--- SubsidyServices/Properties/AssemblyInfo.cs
+++ SubsidyServices/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SubsidyServices")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Subsidy Services")]
+[assembly: AssemblyDescription("Tamkeen")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Tamkeen")]
 [assembly: AssemblyProduct("SubsidyServices")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Tamkeen Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.7.0.0")]
+[assembly: AssemblyFileVersion("1.7.0.0")]
